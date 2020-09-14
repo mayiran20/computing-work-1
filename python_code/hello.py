4103c99cbd86857326d3d00e98d57a51f2c01272
haininggg <69775717+haininggg@users.noreply.github.com>
2020-09-14 09:18:10 +0800

    Update hello.py

--- python_code/hello.py
+++ python_code/hello.py
@@ -1,6 +1,6 @@
-#this should scroll the word potatoes
+#this should scroll the word food
 #but theres an error D:
 #can u figure out how to fix it? :D
 
 from microbit import*
-display.scroll('potatoes)
+display.scroll('food)
